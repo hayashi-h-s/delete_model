6f87b37c9972ba128d5fb1d1505baa235cd48ea7
hayamizhin <max9173627>
2020-02-24 17:04:49 +0900

    user_model_destroy

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-
-  has_many :folders
-
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
-end
