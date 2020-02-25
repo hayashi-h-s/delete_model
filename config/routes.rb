e8a6abf85a17949337e881271fa2377ba0620aee
hayamizhin <max9173627>
2020-02-25 16:42:46 +0900

    [add]deviceの機能確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    # :sessions => 'users/sessions'
+  } 
+
   get 'sessions/new'
   get 'users/new'
   root 'home#top'
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
     # resources :posts
   end
   
-  resources :users
+  resources :users, only: [:index, :show]
   
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_080636) do
+ActiveRecord::Schema.define(version: 2020_02_25_033109) do
 
   create_table "folders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
