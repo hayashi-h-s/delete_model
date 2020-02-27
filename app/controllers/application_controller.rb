c45bff5b73b07c836183aca9b5852f604aeae714
hayamizhin <max9173627>
2020-02-27 13:56:38 +0900

    [add]nameカラムの追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
- include SessionsHelper
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def logged_in_user
     unless logged_in?
       flash[:danger] = "ログインしてください"
       redirect_to login_url
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
 end
